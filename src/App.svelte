d8795728672a2da6bb7ab2aadd49bde33f92740b
Prateek61 <prateekpoudel61@gmail.com>
2023-03-17 21:54:20 +0545

    Svelte setup

---
+++ src/App.svelte
@@ -0,0 +1,12 @@
+<script>
+  import svelteLogo from './assets/svelte.svg'
+  import Counter from './lib/Counter.svelte'
+</script>
+
+<main>
+  <h1>Hello!</h1>
+  <h2>I am Prateek Poudel</h2>
+</main>
+
+<style>
+</style>
